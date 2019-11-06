d8c0b64674c17e81cffddeb285eb183b3d437c74
Martina Scharrer <martina.scharrer@outlook.com>
2019-11-06 23:40:23 +0100

    Coffe cup asset added

--- GraduationSimulator/Assets/Scripts/Collectables/Coffee.cs
+++ GraduationSimulator/Assets/Scripts/Collectables/Coffee.cs
@@ -8,18 +8,26 @@ public class Coffee : Collectable, ILookAtHandler
     private int _coffeeStrength = 10;
     public void OnLookatEnter()
     {
-        this.GetComponent<Renderer>().material.color = Color.yellow;
+        Debug.Log("looking at coffi");        
     }
 
     public void OnLookatExit()
-    {
-        this.GetComponent<Renderer>().material.color = Color.green;
+    {        
     }
 
     public void OnLookatInteraction(Vector3 lookAtPosition, Vector3 lookAtDirection)
     {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
         Destroy(this.gameObject);
         _player.ResetLastLookAtObject();
         _player.IncreaseEnergy(_coffeeStrength);
+    }
+
+    public void ChangeCoffeeStrength(int strength)
+    {
+        _coffeeStrength = strength;
     }
 }
