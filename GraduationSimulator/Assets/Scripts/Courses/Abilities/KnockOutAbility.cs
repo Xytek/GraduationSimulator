e71c30f85fbe232ba719d45fd996d8e461701d0b
Martina Scharrer <martina.scharrer@outlook.com>
2019-12-03 19:15:48 +0100

    deleted icon folder

---
+++ GraduationSimulator/Assets/Scripts/Courses/Abilities/KnockOutAbility.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/KnockOutAbility")]
+public class KnockOutAbility : ScriptableObject
+{
+    public GameObject arm;
+
+}
